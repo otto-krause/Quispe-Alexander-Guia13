67691a07a0bf27a0d5fbf1f72be2b0b8cd30d0df
Alexander <88721696+Alexottokrause@users.noreply.github.com>
2021-10-27 11:28:13 -0300

    Add files via upload

---
+++ ej5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int vn[20]={0};
+	int vdi[15]={0};
+	int vtg=0,vm=0;
+	int i=0,i2=0;
+	for(i=1;i<5;i++)
+	{
+    printf("Vendedor %d\n",i);
+	    for(i2=1;i2<3;i2++){
+            printf("Venta del dia %d:",i2);
+            scanf("%d",&vdi[i]);
+            vtg=vtg+vdi[i];
+            if(vdi[i]>vm){
+                vm=vdi[i];
+            }
+	    }
+	}
+	printf("Venta total:%d\n",vtg);
+    printf("Mayor venta:%d",vm);
+	return 0;
+}
