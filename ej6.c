a28962d6de73e2b91ea818852863e99b6e1ef3bd
Alexander <88721696+Alexottokrause@users.noreply.github.com>
2021-11-02 21:14:52 -0300

    Update ej6.c

--- ej6.c
+++ ej6.c
@@ -1,22 +1,22 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
-	int sl[20]={0};
+	int suel[20]={0};
 	int ct[1]={0};
 	int i;
 	for(i=1;i<21;i++)
     {
 	printf("Ingrese sueldo %d:",i);
-	    scanf("%d",&sl[i]);
-	    if(sl[i]>=2000){
-            ct[0]=ct[0]+1;
+	    scanf("%d",&suel[i]);
+	    if(suel[i]>2000){
+            ct[0]+=1;
 	    }else{
-	        ct[1]=ct[1]+1;
+	        ct[1]+=1;
 	    }
 	}
     printf("Sueldo mayor a 2000:%d\n",ct[0]);
     printf("Sueldo menor a 2000:%d",ct[1]);
 	return 0;
 }
-
