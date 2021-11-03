c001fcf071317047c3e2bff25ee8c58425777412
Alexander <88721696+Alexottokrause@users.noreply.github.com>
2021-11-02 21:39:11 -0300

    Add files via upload

---
+++ ej7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int nt[16]={0};
+	int i;
+	for(i=1;i<16;i++)
+	{
+		printf("Ingrese nota %d: ",i);
+		scanf("%d",&nt[i]);
+	}
+	for(i=1;i<16;i++){
+		if(nt[i]>8){
+			printf("[%d]",i);
+		}
+	}
+	getchar();
+	return 0;
+}
+
+
